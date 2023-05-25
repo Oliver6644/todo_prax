e0025a861f0461fc00f98db47ab3d64538f0638b
Oliver Tkac <olivertkac6644@gmail.com>
2023-05-25 22:43:34 +0200

    ep .42 todo app rendering todo items

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Form from "./components/Form";
 import { useState } from "react";
 import React from "react";
 import Todo from "./components/Todo";
+import TodoList from "./components/TodoList";
 function App () {
   const [todo, setTodo] = useState("");
   const [todoList, setTodoList] = useState([]);
@@ -16,7 +17,7 @@ function App () {
              todoList={todoList} 
              setTodoList={setTodoList}
              ></Form>
-             <Todo />
+             <TodoList todoList={todoList} />
         </div>
     );
 }
