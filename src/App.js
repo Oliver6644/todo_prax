41d6487a210381fc408611a1e2a91de4ef80df0b
Oliver Tkac <olivertkac6644@gmail.com>
2023-05-25 22:34:34 +0200

    ep .41 todo app creating a todo component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Form from "./components/Form";
 import { useState } from "react";
 import React from "react";
+import Todo from "./components/Todo";
 function App () {
   const [todo, setTodo] = useState("");
   const [todoList, setTodoList] = useState([]);
@@ -15,6 +16,7 @@ function App () {
              todoList={todoList} 
              setTodoList={setTodoList}
              ></Form>
+             <Todo />
         </div>
     );
 }
