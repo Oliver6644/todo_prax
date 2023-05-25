1e92d4206f5d255d3ab41f3b545f52967de908c8
Oliver Tkac <olivertkac6644@gmail.com>
2023-05-25 22:55:06 +0200

    ep .44 todo app resolving key error

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,12 +5,12 @@ const Form =({todo,setTodo,todoList,setTodoList })=>{
    
 const handleChange=(event)=>{
     setTodo(event.target.value)
-    console.log(todo);
+    
 };
 const handleSubmit = (event)=>{
     event.preventDefault();
     setTodoList([...todoList, {name:todo,id:shortid.generate()}]);
-    
+    console.log(todoList);
 
 }   
    
